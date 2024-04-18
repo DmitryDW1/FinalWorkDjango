312c6b68bfcb39a35cbe96dbbcbd83ba558d92f1
DnitryDW1 <DmitryDW1@yandex.ru>
2024-04-19 00:42:53 +0600

    Создал приложение

---
+++ mysite/recipe_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Recipe, CategoryRelationship, Category
+
+admin.site.register(Recipe)
+
+admin.site.register(Category)
+
+admin.site.register(CategoryRelationship)
+# Register your models here.
